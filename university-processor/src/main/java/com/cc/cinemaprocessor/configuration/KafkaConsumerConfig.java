fd8866c55f1511ba1f38d07a9a44c73b92f78379
andrei-cune <56235879+andrei-cune@users.noreply.github.com>
2023-01-15 16:51:37 +0200

    Update KafkaConsumerConfig.java

--- university-processor/src/main/java/com/cc/cinemaprocessor/configuration/KafkaConsumerConfig.java
+++ university-processor/src/main/java/com/cc/cinemaprocessor/configuration/KafkaConsumerConfig.java
@@ -1,42 +1,56 @@
-package com.idp.cinema.configuration.kafka;
+package com.cc.cinemaprocessor.configuration;
 
 
-import com.idp.cinema.model.Application;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
+import com.cc.cinemaprocessor.model.Application;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
-public class KafkaProducerConfig {
 
+@EnableKafka
+@Configuration
+@Slf4j
+public class KafkaConsumerConfig {
     private static final String bootstrapAddress = "127.0.0.1:29092";
 
+
+
     @Bean
-    public ProducerFactory<String, Application> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(
-                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+    public ConsumerFactory<String, Application> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
                 bootstrapAddress);
-        configProps.put(
-                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class);
-        configProps.put(
-                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
+        props.put(
+                ConsumerConfig.GROUP_ID_CONFIG,
+                "students");
+        props.put(
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        props.put(
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
     }
 
     @Bean
-    public KafkaTemplate<String, Application> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+    public ConcurrentKafkaListenerContainerFactory<String, Application>
+    kafkaListenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory<String, Application> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
     }
 
 
